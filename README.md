172257eeb0ffd14d8cecc44fcf7ca977415470d1
Luke Chandler <lhchandler4@gmail.com>
2019-05-08 10:06:38 -0700

    initial scaffolding

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+##City Explorer Front End with REACT
+
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Header extends React.Component {
+  render(){
+    return (
+      <header>
+        <h1>City Explorer</h1>
+        <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
       </header>
-    </div>
-  );
+    )
+    }  
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+  } 
+      render() {
+        return (
+          <React.Fragment>
+            <Search />
+            <Map />
+            <Result />
+          </React.Fragment>
+        )  
+      }
+}
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+    }
+
+  // handleSearch = e => {
+    
+  // }
+
+}
+    render() {
+      return (
+        <div>
+          <input onChange={this.handleWord} />
+          <button onClick={this.handleSearch}>Search Location</button>
+        </div>
+      )
+    }
+}
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+    render() {
+      return (
+        <div>
+          <p>Google Maps</p>
+        </div>
+      )
+    }
+}
+
+class Result extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+    render() {
+      return (
+        <div>
+          <div>
+            <p>Dark Sky Weather</p>
+          </div>
+          <div>
+            <p>Event Brite</p>
+          </div>
+          <div>
+            <p>Movie DB</p>
+          </div>
+          <div>
+            <p>Yelp</p>
+          </div>
+          <div>
+            <p>Hiking</p>
+          </div>
+        </div>
+      )
+    }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
+
